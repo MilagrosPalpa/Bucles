6a06c951d87bb61ea0cca4bec1378a2f767329e0
MilagrosPalpa <palpa.milagros@gmail.com>
2022-01-11 22:10:55 -0500

    bucles

---
+++ desafio4.js
@@ -0,0 +1,14 @@
+let suma=0;
+let i=0;
+while(i<=100){
+
+    if(i<100){
+        console.log(i, "+");
+    }
+    else{
+        console.log(i);
+    }
+suma=suma+i;
+i++;
+}
+console.log(suma);
---
+++ desafio5.js
@@ -0,0 +1,14 @@
+let product=1;
+for(let i=1; i<=12; i++){
+    if(i<12){
+        console.log(i, "*");
+    }
+    else{
+        console.log(i);
+    }
+
+    product=product*i;
+    
+}
+
+console.log(product);
